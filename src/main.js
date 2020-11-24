d04403a55a14fd3fa4e9b3a5904deb06cdc96770
Pavel-Liteiniy <paulsev96@gmail.com>
2020-11-24 15:18:56 +0300

    Создает моки фильмов и комментариев

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import {createMenu} from "./view/menu";
 import {createFilmCard} from "./view/film-card";
 import {createContent} from "./view/content";
 import {createMoviesStatistic} from "./view/movies-statistic";
+import {generateFilm} from "./mock/film";
 
+const FILMS_COUNT = 50;
 const CARDS_NUMBER = 5;
 const CARDS_EXTRA_NUMBER = 2;
+
+const films = new Array(FILMS_COUNT).fill().map(generateFilm);
 
 const render = (container, template, position = `beforeend`) => {
   container.insertAdjacentHTML(position, template);
@@ -25,10 +29,12 @@ const filmsListElements = siteMainElement.querySelectorAll(`.films-list`);
 Array.from(filmsListElements).forEach((filmsList) => {
   const filmCardcontainer = filmsList.querySelector(`.films-list__container`);
 
-  let cardsNumber = filmsList.classList.contains(`films-list--extra`) ? CARDS_EXTRA_NUMBER : CARDS_NUMBER;
+  let cardsNumber = filmsList.classList.contains(`films-list--extra`)
+    ? CARDS_EXTRA_NUMBER
+    : CARDS_NUMBER;
 
   for (let i = 0; i < cardsNumber; i++) {
-    render(filmCardcontainer, createFilmCard());
+    render(filmCardcontainer, createFilmCard(films[i]));
   }
 });
 
