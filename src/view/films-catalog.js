748928c5fd9c67d137aa49ddab4001e64f3a6b5d
Pavel-Liteiniy <paulsev96@gmail.com>
2020-12-08 22:12:43 +0300

    Рефакторинг компонента список фильмов

--- src/view/films-catalog.js
+++ src/view/films-catalog.js
@@ -1,13 +1,10 @@
 import AbstractView from "./abstract";
 
 const createFilmsCatalog = (films) => {
-  return films.length > 0 ?
-    `<section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    <div class="films-list__container"></div>
-    </section>`
-    : `<section class="films-list">
-    <h2 class="films-list__title">There are no movies in our database</h2>
+  return `<section class="films-list">
+    ${films.length > 0 ?
+    `<h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>`
+    : `<h2 class="films-list__title">There are no movies in our database</h2>`}
     <div class="films-list__container"></div>
     </section>`;
 };
