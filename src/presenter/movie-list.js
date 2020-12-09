12fe99f05207c951385ed39f40d82d2fb73a06da
Pavel-Liteiniy <paulsev96@gmail.com>
2020-12-09 13:56:38 +0300

    Переносит в презентер код создания подкаталогов c экстра фильмами

--- src/presenter/movie-list.js
+++ src/presenter/movie-list.js
@@ -4,8 +4,15 @@ import FilmsCatalogView from "../view/films-catalog";
 import PopupView from "../view/popup";
 import ShowButtonView from "../view/show-button";
 import MovieCardView from "../view/film-card";
+import TopRatedFilmsView from "../view/top-rated-films";
+import MostCommentedFilmsView from "../view/most-commented-films";
 
 const CARDS_COUNT = 5;
+const CARDS_EXTRA_COUNT = 2;
+const TypeExtraFilms = {
+  MOST_COMMENTED: `most commented`,
+  TOP_RATED: `top rated`
+};
 
 export default class MovieList {
   constructor(filmsContainer) {
@@ -37,6 +44,9 @@ export default class MovieList {
       render(this._filmsCatalogComponent.getElement(), this._showButtonComponent);
       this._showButtonComponent.setClickHandler(this._showButtonClickHandler);
     }
+
+    this._createExtraFilmsElement(this._films, TypeExtraFilms.TOP_RATED);
+    this._createExtraFilmsElement(this._films, TypeExtraFilms.MOST_COMMENTED);
   }
 
   _renderFilmCardElements(container, movies, position = `beforeend`) {
@@ -83,5 +93,44 @@ export default class MovieList {
       this._showButtonComponent.getElement().remove();
       this._showButtonComponent.removeElement();
     }
+  }
+
+  _getTopRatedFilms(movies, quantity) {
+    return movies.slice().sort(({rating: a}, {rating: b}) => {
+      return b - a;
+    }).slice(0, quantity);
+  }
+
+  _getMostCommentedFilms(movies, quantity) {
+    return movies.slice().sort(({comments: a}, {comments: b}) => {
+      return b.length - a.length;
+    }).slice(0, quantity);
+  }
+
+  _renderExtraFilms(componentContainer, movies) {
+    render(this._filmsCardContainer, componentContainer);
+    const componentFilmsContainer = componentContainer.getElement().querySelector(`.films-list__container`);
+
+    if (componentFilmsContainer) {
+      this._renderFilmCardElements(componentFilmsContainer, movies);
+    }
+  }
+
+  _createExtraFilmsElement(movies, typeExtraFilms) {
+    switch (typeExtraFilms) {
+      case TypeExtraFilms.TOP_RATED:
+        this._extraFilms = this._getTopRatedFilms(movies, CARDS_EXTRA_COUNT);
+        this._extraComponent = new TopRatedFilmsView(this._extraFilms);
+        break;
+      case TypeExtraFilms.MOST_COMMENTED:
+        this._extraFilms = this._getMostCommentedFilms(movies, CARDS_EXTRA_COUNT);
+        this._extraComponent = new MostCommentedFilmsView(this._extraFilms);
+        break;
+    }
+
+    this._renderExtraFilms(this._extraComponent, this._extraFilms);
+
+    this._extraFilms = null;
+    this._extraComponent = null;
   }
 }
