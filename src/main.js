965f8a7a74b6df9e9b6125943b55b559c4eb5b63
Pavel-Liteiniy <paulsev96@gmail.com>
2020-12-22 17:09:49 +0300

    Подключает модель фильтра в main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import {render} from "./utils/render";
 import UserRankView from "./view/user-rank";
-import MenuView from "./view/menu";
 import MoviesStatisticView from "./view/movies-statistic";
 import {generateFilm} from "./mock/film";
 import {generateUser} from "./mock/user";
 import MovieListPresenter from "./presenter/movie-list";
+import MenuPresenter from "./presenter/menu";
 import MoviesModel from "./model/movies";
+import FilterModel from "./model/filter";
 
-const FILMS_NUMBER = 20;
+const FILMS_NUMBER = 12;
 const films = new Array(FILMS_NUMBER).fill().map(generateFilm);
+
 const moviesModel = new MoviesModel();
 moviesModel.setMovies(films);
+
+const filterModel = new FilterModel();
 
 const user = generateUser();
 
@@ -20,9 +24,11 @@ const siteMainElement = siteBodyElement.querySelector(`.main`);
 const siteFooterStatisticElement = siteBodyElement.querySelector(`.footer__statistics`);
 
 render(siteHeaderElement, new UserRankView(user));
-render(siteMainElement, new MenuView(user));
 
-const movieListPresenter = new MovieListPresenter(siteMainElement, moviesModel);
+const menuPresenter = new MenuPresenter(siteMainElement, moviesModel, filterModel);
+menuPresenter.init();
+
+const movieListPresenter = new MovieListPresenter(siteMainElement, moviesModel, filterModel);
 movieListPresenter.init();
 
 render(siteFooterStatisticElement, new MoviesStatisticView(films));
