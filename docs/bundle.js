dcf06d16615afdee437768bc191001e5f00fade4
Pavel-Liteiniy <paulsev96@gmail.com>
2021-04-09 23:53:31 +0300

    Правит сборку под Github pages

---
+++ docs/bundle.js
@@ -0,0 +1,279 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _view_user_rank__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./view/user-rank */ "./src/view/user-rank.js");
+/* harmony import */ var _view_menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./view/menu */ "./src/view/menu.js");
+/* harmony import */ var _view_film_card__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./view/film-card */ "./src/view/film-card.js");
+/* harmony import */ var _view_content__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./view/content */ "./src/view/content.js");
+/* harmony import */ var _view_movies_statistic__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./view/movies-statistic */ "./src/view/movies-statistic.js");
+
+
+
+
+
+
+const render = (container, template, position = `beforeend`) => {
+  container.insertAdjacentHTML(position, template);
+};
+
+const siteBodyElement = document.querySelector(`body`);
+const siteHeaderElement = siteBodyElement.querySelector(`.header`);
+const siteMainElement = siteBodyElement.querySelector(`.main`);
+const siteFooterStatisticElement = siteBodyElement.querySelector(`.footer__statistics`);
+
+render(siteHeaderElement, Object(_view_user_rank__WEBPACK_IMPORTED_MODULE_0__["createUserRank"])());
+render(siteMainElement, Object(_view_menu__WEBPACK_IMPORTED_MODULE_1__["createMenu"])());
+render(siteMainElement, Object(_view_content__WEBPACK_IMPORTED_MODULE_3__["createContent"])());
+
+const filmsListElements = siteMainElement.querySelectorAll(`.films-list`);
+
+Array.from(filmsListElements).forEach((filmsList) => {
+  const filmCardcontainer = filmsList.querySelector(`.films-list__container`);
+  let cardsNumber = 5;
+
+  if (filmsList.classList.contains(`films-list--extra`)) {
+    cardsNumber = 2;
+  }
+
+  for (let i = 0; i < cardsNumber; i++) {
+    render(filmCardcontainer, Object(_view_film_card__WEBPACK_IMPORTED_MODULE_2__["createFilmCard"])());
+  }
+});
+
+render(siteFooterStatisticElement, Object(_view_movies_statistic__WEBPACK_IMPORTED_MODULE_4__["createMoviesStatistic"])());
+
+
+/***/ }),
+
+/***/ "./src/view/content.js":
+/*!*****************************!*\
+  !*** ./src/view/content.js ***!
+  \*****************************/
+/*! exports provided: createContent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createContent", function() { return createContent; });
+const createContent = () => {
+  return `<section class="films">
+  <section class="films-list">
+    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+
+    <div class="films-list__container"></div>
+  </section>
+
+  <section class="films-list films-list--extra">
+    <h2 class="films-list__title">Top rated</h2>
+
+    <div class="films-list__container"></div>
+  </section>
+
+  <section class="films-list films-list--extra">
+    <h2 class="films-list__title">Most commented</h2>
+
+    <div class="films-list__container"></div>
+  </section>
+</section>`;
+};
+
+
+/***/ }),
+
+/***/ "./src/view/film-card.js":
+/*!*******************************!*\
+  !*** ./src/view/film-card.js ***!
+  \*******************************/
+/*! exports provided: createFilmCard */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createFilmCard", function() { return createFilmCard; });
+const createFilmCard = () => {
+  return `<article class="film-card">
+  <h3 class="film-card__title">Popeye the Sailor Meets Sindbad the Sailor</h3>
+  <p class="film-card__rating">6.3</p>
+  <p class="film-card__info">
+    <span class="film-card__year">1936</span>
+    <span class="film-card__duration">16m</span>
+    <span class="film-card__genre">Cartoon</span>
+  </p>
+  <img src="./images/posters/popeye-meets-sinbad.png" alt="" class="film-card__poster">
+  <p class="film-card__description">In this short, Sindbad the Sailor (presumably Bluto playing a "role") proclaims himself, in song, to be the greatest sailor, adventurer and…</p>
+  <a class="film-card__comments">0 comments</a>
+  <div class="film-card__controls">
+    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
+    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched film-card__controls-item--active" type="button">Mark as watched</button>
+    <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active" type="button">Mark as favorite</button>
+  </div>
+</article>`;
+};
+
+
+/***/ }),
+
+/***/ "./src/view/menu.js":
+/*!**************************!*\
+  !*** ./src/view/menu.js ***!
+  \**************************/
+/*! exports provided: createMenu */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createMenu", function() { return createMenu; });
+const createMenu = () => {
+  return `<nav class="main-navigation">
+  <div class="main-navigation__items">
+    <a href="#all" class="main-navigation__item">All movies</a>
+    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+    <a href="#history" class="main-navigation__item main-navigation__item--active">History <span class="main-navigation__item-count">4</span></a>
+    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+  </div>
+  <a href="#stats" class="main-navigation__additional">Stats</a>
+</nav>
+
+<ul class="sort">
+  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+  <li><a href="#" class="sort__button">Sort by date</a></li>
+  <li><a href="#" class="sort__button">Sort by rating</a></li>
+</ul>`;
+};
+
+
+/***/ }),
+
+/***/ "./src/view/movies-statistic.js":
+/*!**************************************!*\
+  !*** ./src/view/movies-statistic.js ***!
+  \**************************************/
+/*! exports provided: createMoviesStatistic */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createMoviesStatistic", function() { return createMoviesStatistic; });
+const createMoviesStatistic = () => {
+  return `<p>130 291 movies inside</p>`;
+};
+
+
+/***/ }),
+
+/***/ "./src/view/user-rank.js":
+/*!*******************************!*\
+  !*** ./src/view/user-rank.js ***!
+  \*******************************/
+/*! exports provided: createUserRank */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createUserRank", function() { return createUserRank; });
+const createUserRank = () => {
+  return `<section class="header__profile profile">
+    <p class="profile__rating">Movie Buff</p>
+    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+  </section>`;
+};
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=bundle.js.map
